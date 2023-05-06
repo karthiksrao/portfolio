ecd41f88fb2f7a2c8eabdb3d2369c0f6b3aa7b60
Karthik Rao <me@karthikrao.in>
2023-05-06 14:07:46 -0700

    Theme change

--- downloads/start-download.php
+++
@@ -1,8 +0,0 @@
-<?php
-$download_link = 'KarthikSRao-Portfolio.pdf';
-ob_start();
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="'.basename($download_link).'"');
-header('Content-Length: ' . filesize($download_link));
-readfile($download_link);
-?>
